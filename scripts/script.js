0c20684a924259d522c673bf8ce5120a179b1fff
davidneff403 <149708274+davidneff403@users.noreply.github.com>
2025-04-30 00:35:01 -0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+/*
+Student Name: David Neff
+File Name: script.js
+Date 3/26/25
+*/
+
+//Hamburger menu Function
+function hamburger() {
+	var menu = document.getElementById("menu-links");
+	var logo = document.getElementById("ffc-logo");
+	if (menu.style.display === "block" && logo.style.display === "none") {
+		menu.style.display = "none";
+		logo.style.display = "block";
+	} else {
+		menu.style.display = "block";
+		logo.style.display = "none";
+	}
+}
